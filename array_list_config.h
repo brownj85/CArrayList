0181705054e1752aa4313b5f21bd8f925ca59597
Jack <jb.brown@mail.utoronto.ca>
2019-03-17 20:23:37 -0400

    added config.h file

---
+++ array_list_config.h
@@ -0,0 +1,8 @@
+// Determines the default factor by which
+// an arrayList will be resized 
+#define DEF_RSZ_FCTR 2
+
+
+// Determines the default initial capacity of 
+// an arrayList 
+#define DEF_CAP 1024
---
+++ config.h
@@ -0,0 +1,8 @@
+//default resize factor, i.e the amount by which array capacity
+//will be multiplied during resizing
+#define DEF_RSZ_FCTR 2
+
+//default initial capacity of arrayList
+#define DEF_CAP 1024
+
+
